784415db54b3cdade04489583c57b7d3f8c939f1
cvaleriac <cvaleriac@hotmail.it>
2019-04-17 12:01:24 -0700

    edit, delete fix routes and views

--- MoneyRoller-App/app/controllers/rollovers_controller.rb
+++ MoneyRoller-App/app/controllers/rollovers_controller.rb
@@ -1,49 +1,72 @@
 class RolloversController < ApplicationController
 
-    before_action :authenticate_user!
-    
-
-    def Index
-       @rollovers = Rollover.all
-    end
-
-    def incoming
-      @rollover = Rollover.find_by(id: params[:id])
-    end
-
-      
-    def outgoing
-      @rollover = Rollover.find_by(id: params[:id])
-    end
-
-
-    def new
-      @rollover = Rollover.new
-      @institution = @rollover.build_institution
-    end
+  before_action :authenticate_user!
   
-    def create
+
+  def index
+    if params[:institution_id]
+      @rollovers = Institution.find(params[:institution_id]).rollovers
+    else
+      @rollovers = current_user.rollovers
+    end
     
-      @rollover = current_user.rollovers.build(rollover_params)
+  end
+
+  def incoming
+    @rollover = Rollover.find_by(id: params[:id])
+  end
+
     
-      if @rollover.rollover_type == "Incoming" && @rollover.save
-          redirect_to incoming_path(@rollover)
-      elsif @rollover.rollover_type == "Outgoing" && @rollover.save
-        redirect_to outgoing_path(@rollover)
-      else
-        render :new
+  def outgoing
+    @rollover = Rollover.find_by(id: params[:id])
+  end
+
+
+  def new
+    @rollover = Rollover.new
+    @institution = @rollover.build_institution
+  end
+
+  def create
+  
+    @rollover = current_user.rollovers.build(rollover_params)
+  
+    if @rollover.rollover_type == "Incoming" && @rollover.save
+        redirect_to incoming_path(@rollover)
+    elsif @rollover.rollover_type == "Outgoing" && @rollover.save
+      redirect_to outgoing_path(@rollover)
+    else
+      render :new
+  end
+end
+
+  def edit
+    @rollover = Rollover.find(params[:id])
+  end
+
+  def update
+    @rollover = Rollover.find(params[:id])
+    if @rollover.update(rollover_params) && @rollover.rollover_type == "Incoming"
+      redirect_to incoming_path(@rollover)
+  elsif @rollover.rollover_type == "Outgoing"
+    redirect_to outgoing_path(@rollover)
+    else
+      flash[:notice] = "something went wrong"
+      redirect_to edit_rollover_path(@rollover)
     end
   end
 
-    def edit
-      @rollover = Rollover.find(params[:id])
-    end
-
-    private
-
-    def rollover_params
-      params.require(:rollover).permit(:rollover_type, :origin, :amount, :institution_id, institution_attributes:[:name, :address, :file_type])
-    end
-
+  def destroy
+    @rollover = Rollover.find(params[:id]) 
+    @rollover.destroy
+    flash[:notice] = "Rollover deleted"
+    redirect_to rollovers_path
   end
-            
+
+  private
+
+  def rollover_params
+    params.require(:rollover).permit(:rollover_type, :origin, :amount, :institution_id, institution_attributes:[:name, :address, :file_type])
+  end
+
+end
--- MoneyRoller-App/config/routes.rb
+++ MoneyRoller-App/config/routes.rb
@@ -8,11 +8,20 @@ Rails.application.routes.draw do
   get '/rollovers/index' => 'rollovers#index', as: :index
   get '/rollovers/incoming/:id' => 'rollovers#incoming', as: :incoming
   get '/rollovers/outgoing/:id' => 'rollovers#outgoing', as: :outgoing
-  
-  
-  
-  resources :users, only: [:new, :create]
+  get '/rollovers/incoming/:id/edit' => 'rollovers#edit', as: :editincoming
+  get '/rollovers/outgoing/:id/edit' => 'rollovers#edit', as: :editoutgoing
+
+resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   resources :institutions, only: [:index, :show, :new]
   resources :rollovers, only: [:new, :create, :edit, :destroy]
+
+  resources :institutions, only: [:show] do
+    # nested resource for rollovers
+    resources :rollovers, only: [:incoming, :outgoing, :index]
+  end
+ 
+  resources :rollovers, only: [:index, :incoming, :outgoing, :new, :create, :edit, :update]
+ 
+  root 'rollovers#index'
 end
